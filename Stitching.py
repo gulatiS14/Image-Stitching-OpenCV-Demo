d10d4cc56fc1a5cbe30d3255fcc4e1dbfdeb15e0
Shobhit <144264917+gulatiS14@users.noreply.github.com>
2024-08-27 23:35:03 +0800

    refactor: Improve image stitching algorithm,

--- Stitching.py
+++ Stitching.py
@@ -1,115 +1,54 @@
 import cv2
 import numpy as np
-# np.random.seed(<int>) # you can use this line to set the fixed random seed if you are using np.random
-import random
-# random.seed(<int>) # you can use this line to set the fixed random seed if you are using random
-
 
 def solution(left_img, right_img):
-    """
-    :param left_img:
-    :param right_img:
-    :return: you need to return the result panorama image which is stitched by left_img and right_img
-    """
+    # Convert images to grayscale
+    left_gray = cv2.cvtColor(left_img, cv2.COLOR_BGR2GRAY)
+    right_gray = cv2.cvtColor(right_img, cv2.COLOR_BGR2GRAY)
 
-    key_points1, descriptor1, key_points2, descriptor2 = get_keypoint(left_img, right_img)
-    good_matches = match_keypoint(key_points1, key_points2, descriptor1, descriptor2)
-    final_H = ransac(good_matches)
+    # Detect and compute keypoints and descriptors
+    sift = cv2.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(left_gray, None)
+    kp2, des2 = sift.detectAndCompute(right_gray, None)
 
-    rows1, cols1 = right_img.shape[:2]
-    rows2, cols2 = left_img.shape[:2]
+    # Match keypoints
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
 
-    points1 =  np.float32([[0,0], [0,rows1], [cols1,rows1], [cols1,0]]).reshape(-1,1,2)
-    points  =  np.float32([[0,0], [0,rows2], [cols2,rows2], [cols2,0]]).reshape(-1,1,2)
-    points2 =  cv2.perspectiveTransform(points, final_H)
-    list_of_points = np.concatenate((points1,points2), axis=0)
-
-    [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
-    [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
-
-    H_translation = (np.array([[1, 0, (-x_min)], [0, 1, (-y_min)], [0, 0, 1]])).dot(final_H)
-
-    output_img = cv2.warpPerspective(left_img, H_translation, (x_max-x_min, y_max-y_min))
-    output_img[(-y_min):rows1+(-y_min), (-x_min):cols1+(-x_min)] = right_img
-    result_img = output_img
-    return result_img
-    
-    # TO DO: implement your solution here
-    # raise NotImplementedError
-    
-def get_keypoint(left_img, right_img):
-    l_img = cv2.cvtColor(left_img, cv2.COLOR_BGR2GRAY)
-    r_img = cv2.cvtColor(right_img, cv2.COLOR_BGR2GRAY)
-
-    surf = cv2.xfeatures2d.SURF_create()
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    key_points1 = sift.detect(l_img, None)
-    key_points1, descriptor1 = surf.compute(l_img, key_points1)
-    
-    key_points2 = sift.detect(r_img, None)
-    key_points2, descriptor2 = surf.compute(r_img, key_points2)
-    return key_points1, descriptor1, key_points2, descriptor2
-
-def match_keypoint(key_points1, key_points2, descriptor1, descriptor2):
-    #k-Nearest neighbours between each descriptor
-    i = 0
-    k=2
-    all_matches = []
-    for d1 in descriptor1:
-      dist = []
-      j = 0
-      for d2 in descriptor2:
-          dist.append([i, j, np.linalg.norm(d1 - d2)])
-          j = j + 1
-      dist.sort(key=lambda x: x[2])
-      all_matches.append(dist[0:k])
-      i = i + 1
-
-    # Ratio test to get good matches
+    # Apply ratio test
     good_matches = []
-    for m, n in all_matches:
-        if m[2] < 0.75*n[2]:
-            left_pt = key_points1[m[0]].pt
-            right_pt = key_points2[m[1]].pt
-            good_matches.append(
-                [left_pt[0], left_pt[1], right_pt[0], right_pt[1]])
-    return good_matches
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance:
+            good_matches.append(m)
 
-def homography(points):
-    A = []
-    for pt in points:
-      x, y = pt[0], pt[1]
-      X, Y = pt[2], pt[3]
-      A.append([x, y, 1, 0, 0, 0, -1 * X * x, -1 * X * y, -1 * X])
-      A.append([0, 0, 0, x, y, 1, -1 * Y * x, -1 * Y * y, -1 * Y])
+    # Get matched keypoints
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-    A = np.array(A)
-    u, s, vh = np.linalg.svd(A)
-    H = (vh[-1, :].reshape(3, 3))
-    H = H/ H[2, 2]
-    return H
+    # Find homography matrix
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-def ransac(good_pts):
-    best_inliers = []
-    final_H = []
-    t=5
-    for i in range(5000):
-        random_pts = random.choices(good_pts, k=4)
-        H = homography(random_pts)
-        inliers = []
-        for pt in good_pts:
-            p = np.array([pt[0], pt[1], 1]).reshape(3, 1)
-            p_1 = np.array([pt[2], pt[3], 1]).reshape(3, 1)
-            Hp = np.dot(H, p)
-            Hp = Hp / Hp[2]
-            dist = np.linalg.norm(p_1 - Hp)
+    # Get dimensions of images
+    h1, w1 = left_img.shape[:2]
+    h2, w2 = right_img.shape[:2]
 
-            if dist < t: inliers.append(pt)
+    # Get the canvas dimension
+    pts = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
+    dst = cv2.perspectiveTransform(pts, M)
+    dst = np.concatenate((dst, np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)), axis=0)
 
-        if len(inliers) > len(best_inliers):
-            best_inliers,final_H = inliers,H
-    return final_H
+    [xmin, ymin] = np.int32(dst.min(axis=0).ravel() - 0.5)
+    [xmax, ymax] = np.int32(dst.max(axis=0).ravel() + 0.5)
+    t = [-xmin, -ymin]
+    Ht = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]])  # translate
+
+    # Warp the left image
+    result = cv2.warpPerspective(left_img, Ht.dot(M), (xmax-xmin, ymax-ymin))
+
+    # Overlay the right image
+    result[t[1]:h2+t[1], t[0]:w2+t[0]] = right_img
+
+    return result
 
 if __name__ == "__main__":
     left_img = cv2.imread('left.jpg')
